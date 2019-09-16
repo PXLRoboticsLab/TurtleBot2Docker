383d03667e298a2de6c13edccbfa3c57a84483b4
TimDupontPXL <Tim.Dupont@PXL.be>
2019-09-16 17:00:43 +0200

    Fixed volume

--- 05_turtlebot2/02_start_container.sh
+++ 05_turtlebot2/02_start_container.sh
@@ -6,14 +6,29 @@ xhost +local:docker
 # For non root usage:
 # RUN sudo usermod -a -G video developer
 
-docker run -it \
-    --device /dev/snd \
-    --rm \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XAUTH" \
-    --runtime=nvidia \
-    pxl_ra_ros_kinetic_turtlebox2:latest \
-    bash
+vendor=`glxinfo | grep vendor | grep OpenGL | awk '{ print $4 }'`
+
+if [ $vendor == "NVIDIA" ]; then
+    docker run -it \
+        -v `pwd`/../turtlebot2_src:/home/user/Projects/catkin_ws/src \
+        --device /dev/snd \
+        --rm \
+        --env="DISPLAY" \
+        --env="QT_X11_NO_MITSHM=1" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -env="XAUTHORITY=$XAUTH" \
+        --volume="$XAUTH:$XAUTH" \
+        --runtime=nvidia \
+        pxl_ra_ros_kinetic_turtlebox2:latest \
+        bash
+else
+    docker run --privileged -it \
+        -v `pwd`/../turtlebot2_src:/home/user/Projects/catkin_ws/src \
+        --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+        --device=/dev/dri:/dev/dri \
+        --env="DISPLAY=$DISPLAY" \
+        -e "TERM=xterm-256color" \
+        --cap-add SYS_ADMIN --device /dev/fuse \
+        pxl_ra_ros_kinetic_turtlebox2:latest \
+        bash
+fi
