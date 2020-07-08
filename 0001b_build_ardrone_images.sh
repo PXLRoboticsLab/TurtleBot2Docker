7138281f2ed6cd68b5d9445ad77ae78242e37686
TimDupontPXL <Tim.Dupont@PXL.be>
2020-07-08 23:12:45 +0200

    Added build ardrone only script.

---
+++ 0001b_build_ardrone_images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+vendor=`glxinfo | grep vendor | grep OpenGL | awk '{ print $4 }'`
+
+if [ $vendor == "NVIDIA" ]; then
+    (cd ./01a_nvidia_opengl_cuda; ./01_build_image.sh)
+else
+    (cd ./01b_opengl_direct_rendering; ./01_build_image.sh)
+fi
+
+(cd ./02_ros_kinetic_base; ./01_build_image.sh)
+(cd ./03_ros_kinetic_desktop; ./01_build_image.sh)
+(cd ./04_ros_kinetic_desktop_full; ./01_build_image.sh)
+(cd ./07_ar_drone; ./01_build_image.sh)
